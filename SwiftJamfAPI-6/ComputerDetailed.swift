4df2a1d6de000fa85fef101ac76c0d9d1579167a
ethamoos <amosdeane@gmail.com>
2025-03-28 15:59:30 +0000

    Tweak

--- SwiftJamfAPI-6/ComputerDetailed.swift
+++ SwiftJamfAPI-6/ComputerDetailed.swift
@@ -57,7 +57,7 @@ struct ComputerDetailed: JamfObject {
     guard var components = URLComponents(string: server)
     else {
       throw JamfAPIError.badURL
-    }    
+    }
  
     return components
   }
